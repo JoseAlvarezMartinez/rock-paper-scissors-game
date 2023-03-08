f1f32e9abe7cbb5607000c62fe1367a0c9bc3bc0
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-08 12:41:18 -0300

    --Select Info--

--- src/components/OptionCard.jsx
+++ src/components/OptionCard.jsx
@@ -1,12 +1,15 @@
 const OptionCard = ({ setPick, option }) => {
   return (
-    <div
-      onClick={() => setPick(option.name)}
-      className={`option-card ${option.name == "Scissors" ? "scissors" : ""} ${
-        option.name == "Rock" ? "rock" : ""
-      }`}
-    >
-      <img src={option.image} />
+    <div className="option-container">
+      <div
+        onClick={() => setPick(option.name)}
+        className={`option-card ${
+          option.name == "Scissors" ? "scissors" : ""
+        } ${option.name == "Rock" ? "rock" : ""}`}
+      >
+        <img src={option.image} />
+      </div>
+      <p>{option.name}</p>
     </div>
   );
 };
