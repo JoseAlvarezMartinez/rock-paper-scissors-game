bd86a0ddd5273c29d74162bd4a991c23ee59a3a5
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-03-08 10:05:18 -0300

    --House Pick--

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,46 @@ function App() {
   };
   useEffect(() => {
     if (pick.length) {
-      const random = Math.ceil(Math.random() * 3);
-      if (random === 1) {
-        setOponentPick("Piedra");
-      } else if (random === 2) {
-        setOponentPick("Papel");
-      } else {
-        setOponentPick("TIjeras");
-      }
+      setTimeout(() => {
+        const random = Math.ceil(Math.random() * 3);
+        if (random === 1) {
+          setOponentPick("Rock");
+        } else if (random === 2) {
+          setOponentPick("Paper");
+        } else {
+          setOponentPick("Scissors");
+        }
+      }, 1000);
     }
   }, [pick]);
   return (
-    <>  
+    <>
       <Header score={score} />
       {pick.length ? (
-        <div
-          className={`option-card  ${pick == "Scissors" ? "scissors" : ""} ${
-            pick == "Rock" ? "pick-rock" : ""
-          }`}
-        >
-          <img src={options[pick]} />
+        <div className="pick">
+          <div className="center-pick">
+            <div
+              className={`option-card  ${
+                pick == "Scissors" ? "scissors" : ""
+              } ${pick == "Rock" ? "pick-rock" : ""}`}
+            >
+              <img src={options[pick]} />
+            </div>
+            <h2 className="picked-h2">You Picked</h2>
+          </div>
+
+          <div className="center-pick">
+            <div
+              className={`oponent-card${
+                oponentPick ? "" : "option-card-oponent"
+              }  ${oponentPick == "Scissors" ? "scissors" : ""} ${
+                oponentPick == "Rock" ? "pick-rock" : ""
+              }`}
+            >
+              <img src={options[oponentPick]} />
+            </div>
+            <h2 className="picked-h2">The house picked</h2>
+          </div>
         </div>
       ) : (
         <>
